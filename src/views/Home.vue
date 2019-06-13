fc9854d4f13bce7582926d82b446eb6c2e321090
reza babakhani <babakhani.reza@gmail.com>
2019-06-13 17:25:49 +0430

    Fix conventions

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,42 +1,31 @@
 <template>
   <q-page class="q-pa-lg">
     <q-search
-          class="q-mb-lg"
-          v-model="search" />
+      class="q-mb-lg"
+      v-model="search" />
       <q-card
-          class="q-ma-sm"
-          v-for="item in items"
-          v-bind:key="item"
-          inline
-          style="width: 300px">
+        class="q-ma-sm"
+        v-for="item in calculators"
+        v-bind:key="item"
+        inline
+        style="width: 300px">
         <q-card-media
           class="q-py-lg flex flex-center">
           <q-icon
-          class="text-primary"
-          style="font-size: 100px;"
-          name="place" />
-          </q-card-media>
-          <q-card-title>
-            Cafe Basilico
-            <q-rating slot="subtitle" v-model="stars" :max="5" />
-              <div slot="right" class="row items-center">
-                <q-icon name="place" /> 250 ft
-              </div>
-                </q-card-title>
-                <q-card-main>
-                  <p>$・Italian, Cafe</p>
-                  <p class="text-faded">Small plates, salads & sandwiches in an intimate setting.</p>
-                </q-card-main>
-                <q-card-separator />
-                  <q-card-actions>
-                    <q-btn flat round dense icon="event" />
-                      <q-btn flat label="5:30PM" />
-                        <q-btn flat label="7:30PM" />
-                          <q-btn flat label="9:00PM" />
-                            <q-btn flat color="primary" label="Reserve" />
-                            </q-card-actions>
-                          </q-card>
-                        </q-page>
+            class="text-primary"
+            style="font-size: 100px;"
+            name="place" />
+        </q-card-media>
+        <q-card-title>
+          <router-link :to="{ name: item.name }" >
+          {{ item.name }}
+          </router-link>
+        </q-card-title>
+        <q-card-main>
+          <p class="text-faded">Small plates, salads & sandwiches in an intimate setting.</p>
+        </q-card-main>
+        </q-card>
+  </q-page>
 </template>
 
 <style>
@@ -45,10 +34,14 @@
 <script>
 export default {
   name: 'PageHome',
-  components: {
-  },
   data () {
     return {
+      calculators: [
+        {
+          name: 'BMI',
+          route: 'BMI'
+        }
+      ],
       items: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
       selectOptions: [
         {
