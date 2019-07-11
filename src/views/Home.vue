aafbdfba19bade68dc0c66494da3ac0945e5f4fa
fariborzemami <fariborz.emami1997@gmail.com>
2019-07-11 21:48:28 +0430

    Some changes

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,39 +1,33 @@
 <template>
-  <q-layout>
-    <q-page-container>
-      <q-page class="q-pa-lg">
-        <q-search
-              class="q-mb-lg"
-              v-model="search" />
-          <a>
-            <q-card
-              class="q-ma-sm"
-              v-for="item in calculators"
-              v-bind:key="item"
-              @click="selectItem(item)"
-              inline
-              style="width: 300px">
-              <q-card-media
-              class="q-py-lg flex flex-center">
-                <q-icon
-              class="text-primary"
-              @click="selectItem(item)"
-              style="font-size: 100px;"
-              name="place" />
-                </q-card-media>
-                <q-card-title>
-                  <router-link class="flex flex-center" :to="{ name: item.name }">
-                    {{ item.name }}
-                  </router-link>
-                </q-card-title>
-                <q-card-main>
-                  <p class="text-faded flex flex-center"> {{ item.description }} </p>
-                </q-card-main>
-              </q-card>
-          </a>
-            </q-page>
-        </q-page-container>
-      </q-layout>
+  <q-page class="q-pa-lg">
+    <q-search
+          class="q-mb-lg"
+          v-model="search" />
+      <q-card
+          class="q-ma-sm cursor-pointer"
+          v-for="item in calculators"
+          v-bind:key="item"
+          @click.native="selectItem"
+          inline
+          style="width: 300px">
+        <q-card-media
+          class="q-py-lg flex flex-center">
+          <q-icon
+          class="text-primary"
+          style="font-size: 100px;"
+          name="https://www.freepik.com/free-icon/scale_911024.htm"
+          />
+          </q-card-media>
+          <q-card-title>
+            <router-link class="flex flex-center" :to="{ name: item.name }" >
+              {{ item.name }}
+            </router-link>
+          </q-card-title>
+          <q-card-main>
+            <p class="text-faded flex flex-center"> {{ item.description }} </p>
+          </q-card-main>
+        </q-card>
+      </q-page>
 </template>
 
 <script>
@@ -45,30 +39,30 @@ export default {
         {
           name: 'BMI',
           route: 'BMI',
-          description: 'Calculate Body Mass Index'
+          description: 'Calculate Body Mass Index',
+          icon: 'alarm'
         },
         {
           name: 'GoldenRatio',
           route: 'Golden.Ratio',
-          description: 'Calculate Golden Ratio'
+          description: 'Calculate Golden Ratio',
+          icon: 'done'
         },
         {
           name: 'BaseConverter',
           route: 'Base.Converter',
-          description: 'Number base conversion calculator'
+          description: 'Number base conversion calculator',
+          icon: 'alarm'
         }
       ],
       search: null,
-      select: null,
-      selectedItem: null
+      select: null
     }
   },
   // TODO Fix this
   methods: {
-    selectItem (item) {
-      this.selectedItem = item.route
+    selectItem () {
     }
   }
 }
-
 </script>
