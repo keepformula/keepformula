698c1c2a366566a73751e6860022d5fd6ed471e7
reza babakhani <babakhani.reza@gmail.com>
2019-07-20 19:22:32 +0430

    fix build errors, caused by i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,18 +1,15 @@
+// https://github.com/kazupon/vue-i18n
 import Vue from 'vue'
 import VueI18n from 'vue-i18n'
-import config from '@/config/config'
-import EN from '@/locales/en'
-// import { Validator } from 'vee-validate'
 
 Vue.use(VueI18n)
 const messages = {
-  en: EN.en
+  en: {
+    app_title: 'Shalculator'
+  }
 }
-// https://github.com/kazupon/vue-i18n
 const i18n = new VueI18n({
-  locale: window.localStorage.getItem('locale') ? window.localStorage.getItem('locale') : config.locale,
+  locale: 'en',
   messages
 })
-// const dict = messages.en.validation
-// Validator.localize('en', dict)
 export default i18n
