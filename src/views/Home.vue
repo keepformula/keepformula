7dff997cc42c38b2463c8fd9feefe00c589fddbd
fariborzemami <fariborz.emami1997@gmail.com>
2019-07-31 04:10:30 +0430

    Fix intro layout

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,39 @@
 <template>
   <q-page class="q-pa-md">
+    <div class="row">
     <q-search
-          class="q-mb-md"
+          class="q-my-md q-mx-auto col-12 col-xl-8 col-lg-8 col-md-9 col-sm-10 search-style"
+          hide-underline
           v-model="searchQuery" />
-      <div class="row">
+    </div>
+    <div class="row">
+      <div class="q-mx-auto col-12 col-xl-8 col-lg-8 col-md-9 col-sm-10">
+        <div class="row">
         <div
            v-for="(item, index) in searchResult"
            v-bind:key="item"
-           class="q-pa-sm col-12 col-md-6 col-lg-4 col-xl-3" >
+           class="q-pa-sm col-12 col-md-6 col-lg-4 col-xl-4" >
            <router-link
            :key="index"
            :to="{ name: item.name }" >
            <q-card
-           class="card-hover calculator-cards w-100 q-ma-0 cursor-pointer"
+           class="card-hover calculator-cards w-100 cursor-pointer"
            inline>
              <q-card-media
            class="q-py-lg flex flex-center">
-               <img :src="`/icon/${item.icon}.svg`" width="100" style="width: 100px;"  />
+               <img :src="`/icon/${item.icon}.svg`" style="width: 90px;"  />
              </q-card-media>
              <q-card-title class="text-center">
                {{ item.name }}
              </q-card-title>
           <q-card-main>
-            <p class="text-faded flex flex-center"> {{ item.description }} </p>
+            <p class="text-faded flex flex-center q-caption"> {{ item.description }} </p>
           </q-card-main>
            </q-card>
            </router-link>
         </div>
+        </div>
+      </div>
       </div>
     </q-page>
 </template>
