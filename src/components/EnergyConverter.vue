beb483b281fe8cdbbf830dd9f7a853c4754835d9
fariborzemami <fariborz.emami1997@gmail.com>
2019-08-10 16:17:39 +0430

    Work on html code structure

--- src/components/EnergyConverter.vue
+++ src/components/EnergyConverter.vue
@@ -19,21 +19,21 @@
               <q-card-separator />
                 <q-card-main>
                   <div class="row q-mt-md">
-                    <div class="col-8 col-md-8 col-sm-8 col-lg-6 col-xl-7 col-xs-12 q-mb-md q-pr-md" >
-                      <q-input autofocus type="number" v-model="value" :float-label="$t('value_label')"/>
+                  <div class="col-8 col-md-8 col-sm-8 col-xs-12 q-mb-md q-pr-md" >
+                    <q-input autofocus type="number" v-model="value" :float-label="$t('value_label')"/>
+                  </div>
+                    <div class="col-4 col-lg-4 col-md-4 col-xm-4 col-sm-4 col-xs-12 q-pr-md">
+                      <q-select
+                         v-model="inputUnit"
+                         :float-label="$t('input_unit')"
+                         :options="unit.energy"
+                         :separator="true"
+                         radio
+                         />
                     </div>
-                      <div class="col-4 col-lg-6 col-xl-5 col-md-4 col-xm-4 col-sm-4 col-xs-12 q-pr-md">
-                        <q-select
-                           v-model="inputUnit"
-                           :float-label="$t('input_unit')"
-                           :options="unit.energy"
-                           :separator="true"
-                           radio
-                           />
-                      </div>
-                    <div class="col-8 col-md-8 col-sm-8 col-lg-6 col-xl-7 col-xs-12 q-mb-md q-pr-md" >
-                      </div>
-                      <div class="col-4 col-lg-6 col-xl-5 col-md-4 col-xm-4 col-sm-4 col-xs-12 q-pr-md">
+                  <div class="col-8 col-md-8 col-sm-8 col-xs-12 q-mb-md q-pr-md" >
+                    </div>
+                    <div class="col-4 col-lg-4 col-md-4 col-xm-4 col-sm-4 col-xs-12 q-pr-md">
                           <q-select
                              v-model="outputUnit"
                              :float-label="$t('output_unit')"
