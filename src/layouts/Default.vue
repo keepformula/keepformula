610b93cd5b74e95203def2f9ac4d994813408449
fariborzemami <fariborz.emami1997@gmail.com>
2019-08-12 03:08:20 +0430

    Add bottom border on scroll in HomePage

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,7 +1,9 @@
 <template>
   <q-layout view="lHr Lpr lFf">
-    <div class="row">
-    <q-layout-header class="query q-mx-auto q-pt-lg col-12 col-xl-9 col-lg-9 col-md-10">
+    <q-layout-header
+      class="row" :class="{ 'scroll-header': !showNavbar }"
+      >
+      <div class="query q-mx-auto q-pt-lg col-12 col-xl-9 col-lg-9 col-md-10" >
       <q-toolbar
         color="primary"
         :glossy="$q.theme === 'mat'"
@@ -31,8 +33,8 @@
            />
       </div>
       </q-toolbar>
+      </div>
     </q-layout-header>
-    </div>
     <q-layout-drawer
       v-model="rightDrawerOpen"
       side="right"
@@ -75,11 +77,24 @@ export default {
   name: 'LayoutDefault',
   data () {
     return {
-      rightDrawerOpen: false
+      rightDrawerOpen: false,
+      showNavbar: true,
+      lastScrollPosition: 0,
+      scrollValue: 0
     }
   },
   methods: {
-    openURL
+    openURL,
+    onScroll () {
+      if (Math.abs(window.pageYOffset) < 90) {
+        this.showNavbar = window.pageYOffset < this.lastScrollPosition
+        this.lastScrollPosition = window.pageYOffset
+      }
+    }
+  },
+  mounted () {
+    this.lastScrollPosition = window.pageYOffset
+    window.addEventListener('scroll', this.onScroll)
   }
 }
 </script>
