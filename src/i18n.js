edb8206328229faa90732b063ccee76b87c05e24
reza babakhani <babakhani.reza@gmail.com>
2020-01-30 18:54:31 +0330

    change i18n from typescript to js

---
+++ src/i18n.js
@@ -0,0 +1,12 @@
+// https://github.com/kazupon/vue-i18n
+import Vue from 'vue'
+import VueI18n from 'vue-i18n'
+import En from './locales/en.js'
+
+Vue.use(VueI18n)
+const messages = En
+const i18n = new VueI18n({
+  locale: 'en',
+  messages
+})
+export default i18n
