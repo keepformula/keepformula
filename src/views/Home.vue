bf35a0fa4244d5189a567b6a4df63d22c36c81d8
fariborzemami <fariborz.emami1997@gmail.com>
2020-03-01 13:17:56 +0330

    fix: works on md and lg size UI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,7 +4,7 @@
       <div class="row mobile-search-padding">
         <q-search
            @keydown.enter="oneResult"
-           class="q-mt-md q-mx-auto col-12 col-xl-4 col-lg-6 col-md-10 col-sm-10
+           class="q-mt-sm q-mx-auto col-12 col-xl-4 col-lg-6 col-md-7 col-sm-8
            search-style"
            hide-underline
            autofocus
@@ -18,7 +18,7 @@
           <div
              v-for="(item, index) in searchResult"
              v-bind:key="index"
-             class="q-pa-sm col-6 col-md-4 col-lg-4 col-xl-4 main-card" >
+             class="q-pa-sm col-6 col-sm-6 col-md-4 col-lg-4 col-xl-4 main-card" >
              <router-link
              :key="index"
              :to="{ name: item.name }" >
